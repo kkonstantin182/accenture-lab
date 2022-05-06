9df1f5880cd7d2bedef3e5d302b2706ea404eb84
kkonstantin182 <82477528+kkonstantin182@users.noreply.github.com>
2022-05-06 17:43:18 +0200

    Update ModelUtil.py

--- AddUtil/ModelUtil.py
+++ AddUtil/ModelUtil.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from catboost import CatBoostClassifier
 from sklearn.model_selection import GridSearchCV
 from datetime import datetime
 import os
@@ -9,13 +10,14 @@ import os
 SEED = 42
 
 class HyperTuner():
+    """
+    The сlass for hyperparameter tuning.
+    """
     
     def __init__(self, X, y, model):
 
         self.X = X
         self.y = y
-        #self.X_val = X_val
-        #self.y_val = y_val
         self.model = model
         self.classifier = None
         self.params = None
@@ -24,10 +26,28 @@ class HyperTuner():
     
     @staticmethod
     def get_available_models():
+        """
+        Returns a list of available models.
+            Parameters:
+            -----------
+                None
+            Return:
+            -----------
+                a list of available models.
+        """
 
-        return ('RandomForestClassifier')
+        return (['RandomForestClassifier', 'CatBoostClassifier'])
 
     def _init_classifier_params(self):
+        """
+        Initializes a hyperparameter grid for the selected model.
+            Parameters:
+            -----------
+                None
+            Return:
+            -----------
+                None
+        """
 
         if self.model not in self.get_available_models():
             raise Exception(
@@ -35,7 +55,6 @@ class HyperTuner():
                 "Use 'get_available_models()' to get the list of the available models.")
 
         elif self.model == 'RandomForestClassifier':
-            #print('hui')
 
             self.classifier = RandomForestClassifier(random_state=SEED)
             
@@ -49,16 +68,40 @@ class HyperTuner():
             bootstrap = [True, False]
 
             self.params = {
-                'n_estimators' : n_estimators, # work
-                'criterion' : criterion, # work
-                'max_depth' : max_depth, # work
-                'min_samples_split' : min_samples_split, # work
-                'min_samples_leaf' : min_samples_leaf, # work
-                #'max_features' : max_features, # don't work
+                'n_estimators' : n_estimators, 
+                'criterion' : criterion, 
+                'max_depth' : max_depth, 
+                'min_samples_split' : min_samples_split, 
+                'min_samples_leaf' : min_samples_leaf, 
                 'bootstrap' : bootstrap
             }
 
+        elif self.model == 'CatBoostClassifier':
+
+            self.classifier = CatBoostClassifier(random_seed=SEED)
+
+            max_depth = [1, 3, 5]
+            n_estimators = [100, 200, 300]
+
+            self.params = {
+                'n_estimators' : n_estimators, 
+                'max_depth' : max_depth, 
+            }
+
+
     def fit_predict(self):
+        """
+        Performs a search over specified parameter values for an estimator.
+        Outputs the best set of hyperparameters and corresponding scores on the training and test sets.
+        Saves the results.
+            Parameters:
+            -----------
+                None
+            Return:
+            -----------
+                None
+        """
+        
 
         self._init_classifier_params()
         self.results = {}
@@ -78,6 +121,15 @@ class HyperTuner():
         self._get_best_estimaror()
 
     def _save_results(self):
+        """
+        Saves the results in .npy and .txt formats.
+            Parameters:
+            -----------
+                None
+            Return:
+            -----------
+                None
+        """
 
         abs_path = os.getcwd()
         directory = os.path.join(abs_path, 'hyp_tun_res')
@@ -90,6 +142,15 @@ class HyperTuner():
         np.savetxt(path2, self.best_params_and_results, fmt='%5s', delimiter=',')
         
     def _get_best_estimaror(self):
+        """
+        Outputs the best set of hyperparameters and corresponding scores on the training and test sets.
+            Parameters:
+            -----------
+                None
+            Return:
+            -----------
+                None
+        """
 
         print("The best set of parameteres is: " + str(self.best_params_and_results[0]))
         print("The best test score is : " + str(self.best_params_and_results[1]))
